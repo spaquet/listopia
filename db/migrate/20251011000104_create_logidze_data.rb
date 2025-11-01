6c4e2e4e54b034f3d2b0cfda9e3481968cfa88aa
Stephane Paquet <176050+spaquet@users.noreply.github.com>
2025-11-01 12:32:39 -0700

    Fixing linter warnings

--- db/migrate/20251011000104_create_logidze_data.rb
+++ db/migrate/20251011000104_create_logidze_data.rb
@@ -4,7 +4,7 @@ class CreateLogidzeData < ActiveRecord::Migration[8.0]
   def change
     create_table :logidze_data do |t|
       t.jsonb :log_data
-      t.belongs_to :loggable, polymorphic: true, index: {name: "index_logidze_loggable", unique: true}
+      t.belongs_to :loggable, polymorphic: true, index: { name: "index_logidze_loggable", unique: true }
     end
   end
 end
