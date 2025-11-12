37021b95507b63defc81e0969ac75f0b7f56548f
Stephane Paquet <176050+spaquet@users.noreply.github.com>
2025-11-12 13:58:30 -0800

    Remove .DS_Store from git

--- db/migrate/20250706232501_create_collaborators.rb
+++ db/migrate/20250706232501_create_collaborators.rb
@@ -5,10 +5,13 @@ class CreateCollaborators < ActiveRecord::Migration[8.0]
       t.references :collaboratable, polymorphic: true, null: false, type: :uuid, index: true
       t.references :user, type: :uuid, null: false, foreign_key: true
       t.integer :permission, default: 0, null: false
+      t.string :granted_roles, array: true, default: [], null: false
+      t.jsonb :metadata, default: {}, null: false
 
       t.timestamps
     end
 
     add_index :collaborators, [ :collaboratable_id, :collaboratable_type, :user_id ], unique: true, name: "index_collaborators_on_collaboratable_and_user"
+    add_index :collaborators, :user_id
   end
 end
