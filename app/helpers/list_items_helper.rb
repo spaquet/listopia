0a7c42b4b7a2317794af794545370411345aa69b
Stephane Paquet <176050+spaquet@users.noreply.github.com>
2025-11-13 13:48:10 -0800

    Add '(You)' marker to current user in Assign To dropdown
    - Add format_assignee_name helper to append '(You)' for current_user
    - Use options_from_collection_for_select with lambda for custom formatting
    - Makes it clear which dropdown entry represents the current user
    - Prevents confusion when user name could be ambiguous

--- app/helpers/list_items_helper.rb
+++ app/helpers/list_items_helper.rb
@@ -1,2 +1,10 @@
 module ListItemsHelper
+  # Format user name for dropdown, adding "(You)" marker for current user
+  def format_assignee_name(user)
+    if user == current_user
+      "#{user.name} (You)"
+    else
+      user.name
+    end
+  end
 end
